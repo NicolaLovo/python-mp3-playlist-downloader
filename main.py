359a10a131d9ae8143d0bae81d13fe6e25527f3c
NicolaLovo <nicolalovo@gmail.com>
2022-10-13 21:01:46 +0200

    list conversion

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 import moviepy.editor as mp
 from pytube import Playlist, YouTube
@@ -7,10 +8,6 @@ from pytube.cli import on_progress
 
 def download_playlist_videos(play_list_url, folder):
     playlist = Playlist(play_list_url)
-    try:
-        os.mkdir(folder)
-    except:
-        pass
     i = 1
     for url in playlist:
         try:
@@ -24,12 +21,22 @@ def download_playlist_videos(play_list_url, folder):
             print(f"Error downloading '{url}': {e}")
 
 
+def download_list_videos(videos_urls, folder):
+    i = 1
+    for url in videos_urls:
+        try:
+            yt = YouTube(url, on_progress_callback=on_progress)
+            video = yt.streams.filter(only_audio=True).first()
+            print(
+                f"downloading {i}/{len(videos_urls)}, size {round(video.filesize/(1024*1024))}MB, inside {folder}, '{yt.title}'")
+            video.download(folder, filename=f"{i}-{yt.title}.mp4")
+            i += 1
+        except Exception as e:
+            print(f"Error downloading '{url}': {e}")
+
+
 def convert_videos_to_mp3(video_folder, audio_folder):
     files = os.listdir(video_folder)
-    try:
-        os.mkdir(audio_folder)
-    except:
-        pass
 
     i = 1
     for filename in files:
@@ -53,8 +60,26 @@ def convert_videos_to_mp3(video_folder, audio_folder):
 def main():
     videos_folder = "./download/videos"
     audios_folder = "./download/audio"
-    playlist_url = "https://www.youtube.com/playlist?list=PLrvKqne9ixu1FZTt6afGX3Q8-s0ol1fLs"
-    download_playlist_videos(playlist_url, videos_folder)
+    shutil.rmtree(videos_folder, ignore_errors=True)
+    shutil.rmtree(audios_folder, ignore_errors=True)
+    try:
+        os.mkdir(videos_folder)
+    except:
+        pass
+    try:
+        os.mkdir(audios_folder)
+    except:
+        pass
+
+    # ! videos list
+    videos_list = ["https://www.youtube.com/watch?v=QnMuICr4UmM"]
+    download_list_videos(videos_list, videos_folder)
+
+    # ! playlist
+    # playlist_url = "https://www.youtube.com/playlist?list=PLrvKqne9ixu1FZTt6afGX3Q8-s0ol1fLs"
+    # download_playlist_videos(playlist_url, videos_folder)
+
+    # ! convert videos to mp3
     convert_videos_to_mp3(videos_folder, audios_folder)
 
 
